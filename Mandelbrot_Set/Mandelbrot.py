bab0a922f021b16487093f6ee8949f566461793e
garyg1 <g.gurlask@ufl.edu>
2017-04-14 17:32:52 -0400

    Added Mandelbrot.py.
    Initial upload

---
+++ Mandelbrot_Set/Mandelbrot.py
@@ -0,0 +1,84 @@
+from PIL import Image
+
+MAX_ITERATIONS = 255
+IMAGE_WIDTH, IMAGE_HEIGHT = 192, 108
+LEFT_X, BOTTOM_Y = 0.3, 0.025
+RIGHT_X = 0.3001
+PIXEL_WIDTH = (RIGHT_X - LEFT_X)/IMAGE_WIDTH
+
+def divergenceTest(c, MAX_ITERATIONS):
+
+    '''
+        check if a point [a, b] is in the mandelbrot set
+        return the number of iterations it took to diverge
+        @param c = [a, b], a complex number a + bi
+    '''
+
+    
+    """
+    if abs(c[0]) < 0.005 or abs(c[1]) < 0.005:
+        return -2
+    if c[0] % 0.1 < 0.005 or c[1] % 0.1 < 0.005:
+        return -1
+    """
+    
+    z = [0, 0]
+    iterations = 0
+    while abs(z[0]) < 10 and abs(z[1]) < 10 and iterations < MAX_ITERATIONS:
+        z = [z[0]**2 - z[1]**2 + c[0], 2*z[0]*z[1] + c[1]]
+        iterations += 1
+    return iterations
+
+def fillMandelbrot(image, MAX_ITERATIONS, LEFT_X, BOTTOM_Y, PIXEL_WIDTH):
+    '''
+        fills a PIL Image with the mandelbrot set on the given range
+    '''
+    
+    for x in range(0, IMAGE_WIDTH):
+        for y in range(1, IMAGE_HEIGHT + 1):
+            c = [x*PIXEL_WIDTH + LEFT_X, y*PIXEL_WIDTH + BOTTOM_Y]
+            iterations = divergenceTest(c, MAX_ITERATIONS)
+            
+            color = (0,0,0)
+            if iterations != MAX_ITERATIONS:
+                color = (int(iterations*(255/MAX_ITERATIONS)), int(iterations*(255/MAX_ITERATIONS)), 200)
+            '''
+            if iterations == -1:
+                color = (255, 255, 255)
+            if iterations == -2:
+                color = (255, 0, 0)
+            '''
+            
+            image.putpixel((x, IMAGE_HEIGHT - y), color)
+
+
+def doRegistry():
+    '''
+        reads from registry.txt, which contains the last image id
+        returns last image id + 1, which is the current image id
+        increments the id in registry.txt
+    '''
+    f = open('registry.txt', 'w+')
+    lines = f.readlines()
+    imageNumber = ''
+    if lines == []:
+        imageNumber = '0'
+    else:
+        imageNumber = lines[0]
+    f.close()
+    f = open('registry.txt', 'w+')
+    newImageNumber = int(imageNumber) + 1
+    f.write(str(newImageNumber))
+    f.close()
+    return newImageNumber
+
+
+def main():
+    image = Image.new('RGB', (IMAGE_WIDTH, IMAGE_HEIGHT), 0)
+    fillMandelbrot(image, MAX_ITERATIONS, LEFT_X, BOTTOM_Y, PIXEL_WIDTH)
+
+    imageID = doRegistry()
+    image.save('mandelbrot' + str(imageID) + '.png')
+    image.show()
+
+main()
